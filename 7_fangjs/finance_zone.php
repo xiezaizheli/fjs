6f9f2b46763e6646ccd9b196a6301b9d6f639d51
xiezaizheli <15000734841@163.com>
2017-07-17 09:26:41 +0800

    Initial commit

---
+++ 7_fangjs/finance_zone.php
@@ -0,0 +1,211 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+  <meta charset="UTF-8">
+  <title></title>
+  <link rel="stylesheet" href="css/global.css"/>
+  <link rel="stylesheet" href="css/finance_zone.css"/>
+</head>
+<body>
+<!--顶部异步加载-->
+<?php include_once 'data/header.php' ?>
+
+<!--主体内容-->
+<div class="finance-main">
+  <!--顶部的项目区域划分-->
+  <ul class="market-name">
+    <li class="invest-area">理财项目专区</li>
+  </ul>
+  <div class="invest-content">
+    <a class="one-minute-intro" href="" target="_blank">
+      <img src="img/market/invest1.png">
+    </a>
+    <div class="invest-title">*仅显示近期投资项目</div>
+    <table class="invest-list">
+      <thead>
+      <tr>
+        <th>项目名称</th>
+        <th>预期年化收益率 <i class="up-to"></i> <i class="down-to"></i></th>
+        <th>标的总额（元）<i class="up-to"></i> <i class="down-to"></i></th>
+        <th>投资期限<i class="up-to size-small"></i> <i class="down-to size-small"></i></th>
+        <th>合作机构</th>
+        <th>项目特点</th>
+        <th>投资进度<i class="up-to size-small"></i> <i class="down-to size-small"></i></th>
+        <th>状态</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr>
+        <td class="invest-icon"><span>宝</span>融通宝系列产品第55期</td>
+        <td class="font-red-30"><span>9.20</span>%</td>
+        <td>2,000,000</td>
+        <td>294天</td>
+        <td>
+          <img src="img/market/wjs_logo.png">
+        </td>
+        <td class="invest-char"><span></span>国资背景<span></span>高收益率</td>
+        <td>
+          <canvas id="my-canvas" width="50px" height="50px"></canvas>
+        </td>
+        <td class="invest-state"><span>已结束</span></td>
+      </tr>
+      <tr>
+        <td class="invest-icon"><span>宝</span>融通宝系列产品第55期</td>
+        <td class="font-red-30"><span>9.20</span>%</td>
+        <td>2,000,000</td>
+        <td>294天</td>
+        <td>
+          <img src="img/market/wjs_logo.png">
+        </td>
+        <td class="invest-char"><span></span>国资背景<span></span>高收益率</td>
+        <td>
+          <canvas class="my-canvas" width="50px" height="50px"></canvas>
+        </td>
+        <td class="invest-state"><span>已结束</span></td>
+      </tr>
+      <tr>
+        <td class="invest-icon"><span>宝</span>融通宝系列产品第55期</td>
+        <td class="font-red-30"><span>9.20</span>%</td>
+        <td>2,000,000</td>
+        <td>294天</td>
+        <td>
+          <img src="img/market/wjs_logo.png">
+        </td>
+        <td class="invest-char"><span></span>国资背景<span></span>高收益率</td>
+        <td>
+          <canvas class="my-canvas" width="50px" height="50px"></canvas>
+        </td>
+        <td class="invest-state"><span>已结束</span></td>
+      </tr>
+      <tr>
+        <td class="invest-icon"><span>宝</span>融通宝系列产品第55期</td>
+        <td class="font-red-30"><span>9.20</span>%</td>
+        <td>2,000,000</td>
+        <td>294天</td>
+        <td>
+          <img src="img/market/wjs_logo.png">
+        </td>
+        <td class="invest-char"><span></span>国资背景<span></span>高收益率</td>
+        <td>
+          <canvas class="my-canvas" width="50px" height="50px"></canvas>
+        </td>
+        <td class="invest-state"><span>已结束</span></td>
+      </tr>
+      <tr>
+        <td class="invest-icon"><span>宝</span>融通宝系列产品第55期</td>
+        <td class="font-red-30"><span>9.20</span>%</td>
+        <td>2,000,000</td>
+        <td>294天</td>
+        <td>
+          <img src="img/market/wjs_logo.png">
+        </td>
+        <td class="invest-char"><span></span>国资背景<span></span>高收益率</td>
+        <td>
+          <canvas class="my-canvas" width="50px" height="50px"></canvas>
+        </td>
+        <td class="invest-state"><span>已结束</span></td>
+      </tr>
+      <tr>
+        <td class="invest-icon"><span>宝</span>融通宝系列产品第55期</td>
+        <td class="font-red-30"><span>9.20</span>%</td>
+        <td>2,000,000</td>
+        <td>294天</td>
+        <td>
+          <img src="img/market/wjs_logo.png">
+        </td>
+        <td class="invest-char"><span></span>国资背景<span></span>高收益率</td>
+        <td>
+          <canvas class="my-canvas" width="50px" height="50px"></canvas>
+        </td>
+        <td class="invest-state"><span>已结束</span></td>
+      </tr>
+      <tr>
+        <td class="invest-icon"><span>宝</span>融通宝系列产品第55期</td>
+        <td class="font-red-30"><span>9.20</span>%</td>
+        <td>2,000,000</td>
+        <td>294天</td>
+        <td>
+          <img src="img/market/wjs_logo.png">
+        </td>
+        <td class="invest-char"><span></span>国资背景<span></span>高收益率</td>
+        <td>
+          <canvas class="my-canvas" width="50px" height="50px"></canvas>
+        </td>
+        <td class="invest-state"><span>已结束</span></td>
+      </tr>
+      <tr>
+        <td class="invest-icon"><span>宝</span>融通宝系列产品第55期</td>
+        <td class="font-red-30"><span>9.20</span>%</td>
+        <td>2,000,000</td>
+        <td>294天</td>
+        <td>
+          <img src="img/market/wjs_logo.png">
+        </td>
+        <td class="invest-char"><span></span>国资背景<span></span>高收益率</td>
+        <td>
+          <canvas class="my-canvas" width="50px" height="50px"></canvas>
+        </td>
+        <td class="invest-state"><span>已结束</span></td>
+      </tr>
+      <tr>
+        <td class="invest-icon"><span>宝</span>融通宝系列产品第55期</td>
+        <td class="font-red-30"><span>9.20</span>%</td>
+        <td>2,000,000</td>
+        <td>294天</td>
+        <td>
+          <img src="img/market/wjs_logo.png">
+        </td>
+        <td class="invest-char"><span></span>国资背景<span></span>高收益率</td>
+        <td>
+          <canvas class="my-canvas" width="50px" height="50px"></canvas>
+        </td>
+        <td class="invest-state"><span>已结束</span></td>
+      </tr>
+      <tr>
+        <td class="invest-icon"><span>宝</span>融通宝系列产品第55期</td>
+        <td class="font-red-30"><span>9.20</span>%</td>
+        <td>2,000,000</td>
+        <td>294天</td>
+        <td>
+          <img src="img/market/wjs_logo.png">
+        </td>
+        <td class="invest-char"><span></span>国资背景<span></span>高收益率</td>
+        <td>
+          <canvas class="my-canvas" width="50px" height="50px"></canvas>
+        </td>
+        <td class="invest-state"><span>已结束</span></td>
+      </tr>
+      <tr>
+        <td class="invest-icon"><span>宝</span>融通宝系列产品第55期</td>
+        <td class="font-red-30"><span>9.20</span>%</td>
+        <td>2,000,000</td>
+        <td>294天</td>
+        <td>
+          <img src="img/market/wjs_logo.png">
+        </td>
+        <td class="invest-char"><span></span>国资背景<span></span>高收益率</td>
+        <td>
+          <canvas class="my-canvas" width="50px" height="50px"></canvas>
+        </td>
+        <td class="invest-state"><span>已结束</span></td>
+      </tr>
+      </tbody>
+    </table>
+    <ul class="invest-page">
+      <li><a class="prev-page" href="">上一页</a></li>
+      <li><a href="">1</a></li>
+      <li><a class="current" href="">2</a></li>
+      <li><a href="">3</a></li>
+      <li><a href="">4</a></li>
+      <li><a href="">5</a></li>
+      <li><a class="next-page" href="">下一页</a></li>
+    </ul>
+  </div>
+</div>
+
+<!--异步加载，底部copyright部分-->
+<?php include_once 'data/footer.php' ?>
+<script src="js/jquery-1.11.3.js"></script>
+<script src="js/finance_zone.js"></script>
+</body>
+</html>
